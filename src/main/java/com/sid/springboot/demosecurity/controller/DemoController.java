1b14570a17b033448179d76a60fb53aac0b416d3
sidharth <sidharthm673@gmail.com>
2024-07-12 01:09:02 +0530

    restrict URL's to user roles

--- src/main/java/com/sid/springboot/demosecurity/controller/DemoController.java
+++ src/main/java/com/sid/springboot/demosecurity/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.sid.springboot.demosecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
@@ -12,4 +13,17 @@ public class DemoController {
         return "home";
     }
 
+    // add request mapping for /leaders
+    @GetMapping("/leaders")
+    public String showLeaders(){
+
+        return "leaders";
+    }
+
+    // add request mapping for /systems
+    @GetMapping("/systems")
+    public String showAdmins(){
+
+        return "systems";
+    }
 }
