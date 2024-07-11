f5a6e5fee201bee2934b7db6fc04a546b5caaa5d
sidharth <sidharthm673@gmail.com>
2024-07-11 15:43:53 +0530

    home page

--- src/main/java/com/sid/springboot/demosecurity/controller/DemoController.java
+++ src/main/java/com/sid/springboot/demosecurity/controller/DemoController.java
@@ -1,4 +1,15 @@
 package com.sid.springboot.demosecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DemoController {
+
+    @GetMapping("/")
+    public String showHome(){
+
+        return "home";
+    }
+
 }
