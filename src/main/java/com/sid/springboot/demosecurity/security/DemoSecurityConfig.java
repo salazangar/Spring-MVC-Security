17c00e6a0b477d3e28d96defee44eda30da583e7
sidharth <sidharthm673@gmail.com>
2024-07-12 14:38:35 +0530

    custom tables for authentication

--- src/main/java/com/sid/springboot/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/sid/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -16,7 +16,19 @@ public class DemoSecurityConfig {
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define a query to retrieve user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id,pw,active from members where user_id=?"
+        );
+
+        // define a query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id,roles from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
